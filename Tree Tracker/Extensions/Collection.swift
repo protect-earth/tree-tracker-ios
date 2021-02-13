9366f7b02eac9593761f7ef5619404acca18a76d
Łukasz Mróz <thesunshinejr@gmail.com>
2021-02-13 17:25:44 +0100

    Add some sorting options with optionals based on partition

--- Tree Tracker/Extensions/Collection.swift
+++ Tree Tracker/Extensions/Collection.swift
@@ -24,4 +24,39 @@ extension Collection {
             }
         })
     }
+
+    fileprivate func unsafeSorted<T: Comparable>(by keyPath: KeyPath<Element, T?>, order: CollectionSortingOrder) -> [Element] {
+        return sorted(by: {
+            switch order {
+            case .ascending:
+                return $0[keyPath: keyPath]! <= $1[keyPath: keyPath]!
+            case .descending:
+                return $0[keyPath: keyPath]! >= $1[keyPath: keyPath]!
+            }
+        })
+    }
+}
+
+extension Array {
+    func sorted<T: Comparable>(by keyPath: KeyPath<Element, T?>, order: CollectionSortingOrder, fallback: KeyPath<Element, T>, fallbackOrder: CollectionSortingOrder) -> [Element] {
+        guard count > 1 else { return self }
+        
+        var copy = self
+        let fallbackFirstIndex = copy.partition { $0[keyPath: keyPath] == nil }
+        let firstPart = copy[0...fallbackFirstIndex - 1].unsafeSorted(by: keyPath, order: order)
+        let secondPart = copy[fallbackFirstIndex...].sorted(by: fallback, order: fallbackOrder)
+
+        return firstPart + secondPart
+    }
+
+    func sorted<T: Comparable>(by keyPath: KeyPath<Element, T?>, order: CollectionSortingOrder) -> [Element] {
+        guard count > 1 else { return self }
+
+        var copy = self
+        let fallbackFirstIndex = copy.partition { $0[keyPath: keyPath] == nil }
+        let firstPart = copy[0...fallbackFirstIndex - 1].unsafeSorted(by: keyPath, order: order)
+        let secondPart = copy[fallbackFirstIndex...]
+
+        return firstPart + secondPart
+    }
 }
