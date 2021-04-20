238d5b98a8be26b2dfafd1dcea42cdfc4fdd3269
Łukasz Mróz <thesunshinejr@gmail.com>
2021-04-20 20:23:54 +0200

    Small comment regarding DelayedPublished wrapper

--- Tree Tracker/Utilities/Property Wrappers/DelayedPublished.swift
+++ Tree Tracker/Utilities/Property Wrappers/DelayedPublished.swift
@@ -2,6 +2,7 @@ import Combine
 import Foundation
 
 @propertyWrapper
+// This is not 1-to-1 @Published copy since it's not updating underlying ObservableObjectPublisher like @Published does
 final class DelayedPublished<Value> {
     typealias Publisher = AnyPublisher<Value, Never>
 
