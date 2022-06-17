08a94f98b907569ef0c2b28b90d2239a188ba890
Allan Lang <lang.allan@gmail.com>
2022-06-17 17:00:37 +0100

    Basic Rollbar integration

--- Tree Tracker/AppDelegate.swift
+++ Tree Tracker/AppDelegate.swift
@@ -1,10 +1,19 @@
 import UIKit
+import RollbarNotifier
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let config = RollbarConfig()
+        config.destination.accessToken = Secrets.rollbarAuthToken
+        config.destination.environment = "local"
+        config.setServerHost(nil, root: nil, branch: nil, codeVersion: "0.8.2")
+        
+        Rollbar.initWithConfiguration(config)
+        Rollbar.infoMessage("See this message on your Rollbar Project Dashboard...")
+        
         window = UIWindow()
         window?.rootViewController = MainFlowViewController()
         window?.makeKeyAndVisible()
