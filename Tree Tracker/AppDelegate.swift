476139d9a512ed55334a506ca3596a0124fa1611
Allan Lang <allan.lang@pwc.com>
2022-07-06 09:13:16 +0100

    Remove extraneous config settings

--- Tree Tracker/AppDelegate.swift
+++ Tree Tracker/AppDelegate.swift
@@ -8,9 +8,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let config = RollbarConfig()
         config.destination.accessToken = Secrets.rollbarAuthToken
-        config.destination.environment = "local"
-        // TODO: Get app version from Bundle info dictionary
-        config.setServerHost(nil, root: nil, branch: nil, codeVersion: "0.8.2")
         
         Rollbar.initWithConfiguration(config)
         Rollbar.infoMessage("App startup")
