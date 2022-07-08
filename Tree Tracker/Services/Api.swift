76bb678aa9874971e1284a132e1da9d83d349d10
Allan Lang <lang.allan@gmail.com>
2022-07-08 19:25:51 +0100

    Dead code removal

--- Tree Tracker/Services/Api.swift
+++ Tree Tracker/Services/Api.swift
@@ -3,11 +3,6 @@ import Alamofire
 import class UIKit.UIImage
 
 protocol Api {
-    func treesPlanted(offset: String?, completion: @escaping (Result<Paginated<AirtableTree>, AFError>) -> Void)
-    func species(offset: String?, completion: @escaping (Result<Paginated<AirtableSpecies>, AFError>) -> Void)
-    func sites(offset: String?, completion: @escaping (Result<Paginated<AirtableSite>, AFError>) -> Void)
-    func supervisors(offset: String?, completion: @escaping (Result<Paginated<AirtableSupervisor>, AFError>) -> Void)
     func upload(tree: LocalTree, progress: @escaping (Double) -> Void, completion: @escaping (Result<AirtableTree, AFError>) -> Void) -> Cancellable
     func loadImage(url: String, completion: @escaping (UIImage?) -> Void)
-    func addSite(name: String, completion: @escaping (Result<AirtableSite, AFError>) -> Void)
 }
