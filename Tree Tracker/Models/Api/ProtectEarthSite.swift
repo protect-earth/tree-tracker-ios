e924c0c0975b3785c268c6e107db1d62fe4de9c2
Allan Lang <lang.allan@gmail.com>
2022-07-18 23:21:20 +0100

    Remove unnecessary Site fields

--- Tree Tracker/Models/Api/ProtectEarthSite.swift
+++ Tree Tracker/Models/Api/ProtectEarthSite.swift
@@ -4,11 +4,6 @@ import CoreLocation
 struct ProtectEarthSite: Codable {
     let id: String
     let name: String
-    let location: String
-    let url: URL?
-    let plantedTrees: Int
-    // TODO: Do we want to handle coordinates?
-//    let coordinates: CLLocationCoordinate2D
     
     func toSite() -> Site {
         return .init(id: id, name: name)
